7f20841ccd390cfcb4e0078bae380718ac5b3ab2
Oltiariq3561 <oltiariq356@gmail.com>
2024-09-26 18:20:02 -0700

    hbfh

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import MyFormData from './components/MyFormData'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <main className="grow">
+      <MyFormData/>
+    </main>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const brands = ['Merc','Mercedes-benz','Mercedes','GM']
+export const colors = ['black','red','white','gray']
